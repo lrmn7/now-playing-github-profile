1304fdb7ad31fc111d2cb748801acdaf97e6deb8
Arta Abedi Firouzjaei <Hey@artaabedi.com>
2024-09-12 17:52:20 +0330

    Update spotify.ts

--- src/spotify.ts
+++ src/spotify.ts
@@ -1,8 +1,8 @@
 import SpotifyWebApi from "spotify-web-api-node";
 
 export const spotifyAPI = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  clientId: process.env.b1d29ba24e024655a07b78a5c1dcb287,
+  clientSecret: process.env.20a2ea65172c451b888bccef138f2429,
   refreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
   redirectUri: `${process.env.BASE_URL}/api/login-callback`,
 });
